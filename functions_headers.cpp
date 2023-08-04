a68cbba37b05e40df808340e0a6bd4da3fce55fb
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-04 14:34:35 +0200

    Add headers and protocols

---
+++ functions_headers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "functions_utils.h"
+
+int main() {
+  showMenu();
+  int input = getInput();
+	processInput(input);
+
+  return 0;
+}
+
+
---
+++ functions_utils.h
@@ -0,0 +1,39 @@
+#ifndef FUNCTIONS_UTILS_H_
+#define FUNCTIONS_UTILS_H_
+
+#include <iostream>
+void showMenu() {
+
+	// show the menu
+	std::cout << "1. Search" << std::endl;
+	std::cout << "2. View Record" << std::endl;
+	std::cout << "3. Quit" << std::endl;
+}
+
+int getInput() {
+	// Get Input
+	std::cout << "Enter your selection > " << std::endl;
+	int value;
+	std::cin >> value;
+	return value;
+}
+
+void processInput(int value) {
+
+	// Make a decision based on the input using switch
+	switch (value) {
+	case 1:
+		std::cout << "Searching ..." << std::endl;
+		break;
+	case 2:
+		std::cout << "Viewing ..." << std::endl;
+		break;
+	case 3:
+		std::cout << "Quitting ..." << std::endl;
+		break;
+	default:
+		std::cout << "Invalid option." << std::endl;
+	}
+}
+
+#endif /* FUNCTIONS_UTILS_H_ */
