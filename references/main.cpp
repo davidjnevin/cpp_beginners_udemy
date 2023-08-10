3fb11d47137db4947cf19cf966ba244fd08838e4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-10 22:04:49 +0200

    Add references examples

---
+++ references/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#define LOG(x) std::cout << x << std::endl
+
+// Pass by reference
+void addOne(int& value) {
+	value++;
+}
+
+int main() {
+
+	int value1 = 5;
+	int& value2 = value1;
+
+	LOG(value1);
+	LOG(value2);
+
+	int value3 = 10;
+	LOG(value3);
+	addOne(value3);
+	LOG(value3);
+  return 0;
+}
